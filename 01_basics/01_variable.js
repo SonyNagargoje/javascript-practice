494b80aa0ae062c746a424db18ec0326a1f7e963
SonyNagargoje <126774966+SonyNagargoje@users.noreply.github.com>
2023-10-13 17:41:21 +0530

    Create 01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail ="sonybangar@gmail.com"
+var accountPassword = "Pune"
+
+// account= 2// not allowed
+
+accountEmail= "sony@gmail.com"
+accountPassword = "122322"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+
+/*
+prefer not to use var
+because of issue in block scope and function scope */
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
