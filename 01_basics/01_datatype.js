db9533725c2590bca30cc614d424bc35c839936a
SonyNagargoje <126774966+SonyNagargoje@users.noreply.github.com>
2023-10-13 17:42:10 +0530

    Create 01_datatype.js

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3)// we are using nodejs.not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+    console.log("sony")
+
+    let name ="sony"
+    let age =18
+    let isLoggedIn = false
+    let state;
+
+    
+    /* primitives data type */
+    //number => 2 to power 53
+    //bigint
+   //string=>""
+   //boolean => true/false
+   //null => standalone value
+   //undefined =>
+   //symbole => unique
+
+
+
+   //object
+
+   console.log(typeof "sony");
+   console.log(typeof  null);  //type of null is object 
+   console.log(typeof  undefined); //undefined
