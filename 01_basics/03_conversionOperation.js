6d9273ac58ecc901ee9c2c27674f9b5c53b4b8dd
SonyNagargoje <126774966+SonyNagargoje@users.noreply.github.com>
2023-10-13 17:42:59 +0530

    Create 03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+let score = "sony"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//conversion 
+//if "33" conersion is =>33
+//"33abc"  conersion is =>NAN
+//true   conersion is => 1; false=>0
+
+let isLoggedIn = "sony"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//conversion
+//if 1 conversion is => true; if 0 => false
+//if " " conversion is =>alse
+//if "sony"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+//******** operation******* */
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+let str1 ="hello"
+let str2 =" sony"
+ 
+
+let str3= str1 + str2
+//console.log(str3);
+
+/*console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); if string is first then as it is print vaue
+console.log(1 + 2 + "2"); //  if string is last then first 2 value add and string is defalt*/
+
+//console.log(+true);//outeput is 1
+//console.log(true+);//create error
+//console.log(+"");
+
+//let num1, num2, num3
+
+//num1 = num2  = num3 = 2+2  //every num is 4
+
+let gameCounter =100
+gameCounter++;// increment by 1
+console.log(gameCounter);
+//link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
