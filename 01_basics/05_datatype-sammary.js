6ee4d4616c5d1fbe5b46b28c355663f65e2b13c7
SonyNagargoje <126774966+SonyNagargoje@users.noreply.github.com>
2023-10-14 15:45:20 +0000

    datasammary

--- 01_basics/05_datatype-sammary.js
+++ 01_basics/05_datatype-sammary.js
@@ -9,7 +9,7 @@
  const scoreValue = 100.3  // in js desimal also allow in number datatype
  
  const isLoggedIn = false //bolean
- const  = null //empty , if you check  data type  it returns object becse assign null value
+ const outsideTemp  = null //empty , if you check  data type  it returns object becse assign null value
  let userEmail;  //undefine
  const id = Symbol('123')
  const anotherId = Symbol('123')
@@ -32,4 +32,35 @@
  
   console.log(typeof outsideTemp);
  //all non primitive data type returns type is always  function but it tell the object function
-  
+
+
+
+
+ //***********memory related*************
+
+ //memory is two type stack and heap memory
+//  stack memory (primitive) means all primitivedata type store in stack
+//   heap memory (non primitive) means all non primitive data types are store in heap memory
+//   in heap emory give a reference by value
+
+let myYoutubename = "sonybangardotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log( myYoutubename);//output is sonybangardotcom
+console.log(anothername); //chaiaurcode two output is defferent beacause this is the primitive data type
+                         //in primitive data type gives copy of value 
+
+let userOne={
+  email: "user@google.com",
+  upi:"user@ybl"
+
+}
+let userTwo =  userOne
+
+userTwo.email = "sony@google.com"
+
+console.log(userOne.email); //outeput is sony@goole.com
+console.log(userTwo.email); //sony@goole.com  two rwsult is same because this is heap memory
+                        //in heap memory gives reference of vlue
