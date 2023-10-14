0fea027227c214d947d31a6fb426a50b4e825f55
SonyNagargoje <126774966+SonyNagargoje@users.noreply.github.com>
2023-10-14 12:30:03 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);  //this are basic simpal and comman comparis
+
+
+// console.log("2" >1); // this two comparision are confusing comparision  
+// console.log("02" >1);  //in this comparision string atomaticaly convert in number
+
+// console.log(null > 0);  //thos result is false because null is empty value 
+// console.log(null == 0); // this result is false
+// console.log(null >= 0); // this result is true because null is convert in number treating it as 0
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+
+// strict check means ===
+
+console.log( "2"===2); // its not only value check its check data type also if == it true if === its not conversion so ans is false because datatype is diff
+
+
+
